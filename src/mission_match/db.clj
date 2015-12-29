56c37127e56471b44dcfb1dad2627b84aa240230
slightlycyborg <taggartmachines@gmail.com>
2015-12-29 02:29:48 -0500

    missions test deletes mission after testing

--- src/mission_match/db.clj
+++ src/mission_match/db.clj
@@ -1,60 +1,61 @@
-    (ns mission-match.db
-      (:require [monger.core :as mg]
-                [monger.collection :as mc]
-                [com.stuartsierra.component :as component]))
+(ns mission-match.db
+  (:require [monger.core :as mg]
+            [monger.collection :as mc]
+            [com.stuartsierra.component :as component]))
 
 
-    (defrecord Connection []
-      component/Lifecycle
+(defrecord Connection []
+  component/Lifecycle
 
-      (start [component]
-        (println "Starting Connectio")
-        (let [conn (mg/connect) ]
-          (assoc component :conn conn)))
-      
-      (stop [component]
-        (println "Stopping Database")))
-
-
-    (defrecord Database [conn]
-      component/Lifecycle
-
-      (start [component]
-        (println "Starting Database")
-        (let [db (mg/get-db (:conn conn) "mission-match")]
-          (assoc component :db db))))
-
-    (defn new-connection  []
-        (map->Connection  {}))
-
-    (defn new-database  []
-        (map->Database  {}))
-
-    (defn db-system []
-      (component/system-map
-        :conn (new-connection)
-        :db (component/using
-              (new-database)
-              [:conn])))
-
-    (def sys (atom (db-system)))
-    
-    (defn start! []
-      (reset! sys (component/start @sys)))
-
-    (defn stop! []
-      (reset! sys (component/stop @sys)))
-
-    (start!)
+  (start [component]
+    (println "Starting Connectio")
+    (let [conn (mg/connect) ]
+      (assoc component :conn conn)))
+  
+  (stop [component]
+    (println "Stopping Database")))
 
 
-    (defn get-db []
-      (:db (:db @sys)))
+(defrecord Database [conn]
+  component/Lifecycle
 
-    (defn test-db-system []
-      (mc/insert (get-db) "test" {:text "this is a db-system-test"})
-      (if
-        (= (:text (mc/find-one-as-map (get-db) "test" {}))
-          "this is a db-system-test")
-        (println "db-system-test passes")
-        (println "db-system-test fails")))
+  (start [component]
+    (println "Starting Database")
+    (let [db (mg/get-db (:conn conn) "mission-match")]
+      (assoc component :db db))))
+
+(defn new-connection  []
+    (map->Connection  {}))
+
+(defn new-database  []
+    (map->Database  {}))
+
+(defn db-system []
+  (component/system-map
+    :conn (new-connection)
+    :db (component/using
+          (new-database)
+          [:conn])))
+
+(def sys (atom (db-system)))
+
+(defn start! []
+  (reset! sys (component/start @sys)))
+
+(defn stop! []
+  (reset! sys (component/stop @sys)))
+
+(start!)
+
+
+(defn get-db []
+  (:db (:db @sys)))
+
+(defn test-db-system []
+  (mc/insert (get-db) "test" {:text "this is a db-system-test"})
+  (if
+    (= (:text (mc/find-one-as-map (get-db) "test" {}))
+      "this is a db-system-test")
+    (println "db-system-test passes")
+    (println "db-system-test fails"))
+  (mc/remove (get-db) "test"))
--- src/mission_match/models/missions.clj
+++ src/mission_match/models/missions.clj
@@ -1,25 +1,24 @@
 (ns mission-match.models.missions
     (:require  [monger.core :as mg]
-               [monger.collection :as mc])
+               [monger.collection :as mc]
+               [mission-match.db :as db])
      (:import org.bson.types.ObjectId)) 
-
-(require '[mission-match.db :refer [db]])
-
 
 (defn existence [] "I exist")
 
 (defn insert-mission [args]
-  (mc/insert db "missions" {:_id (ObjectId.) 
+  (mc/insert (db/get-db) "missions" {:_id (ObjectId.) 
                             :text (args :text) 
                             :user-id (args :user-id)}))
 
 (defn get-mission-by-user [user-id]
-  (mc/find-one-as-map db "missions" {:user-id user-id}))
+  (mc/find-one-as-map (db/get-db) "missions" {:user-id user-id}))
 
 
 (defn test-insert-mission []
   (insert-mission {:text "My mission is to code" :user-id 1})
   (if (= "My mission is to code" (:text (get-mission-by-user 1)))
          (println "Passes")
-         (println "Fails")))
+         (println "Fails"))
+  (mc/remove-by-id (db/get-db) "missions" (:_id (get-mission-by-user 1))))
 
