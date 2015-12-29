0ec783538b1d43611c98a26821c3df45180ac78d
slightlycyborg <taggartmachines@gmail.com>
2015-12-29 02:11:45 -0500

    db component added

--- src/mission_match/db.clj
+++ src/mission_match/db.clj
@@ -1,21 +1,60 @@
-(ns mission-match.db
-  (:require [monger.core :as mg]
-            [com.stuartsierra.component :as component]))
+    (ns mission-match.db
+      (:require [monger.core :as mg]
+                [monger.collection :as mc]
+                [com.stuartsierra.component :as component]))
 
-(defstate conn
-    :start  mg/connect 
-    :stop   mg/disconnect)
 
-(use '[mission-match.db :refer [conn]])
+    (defrecord Connection []
+      component/Lifecycle
 
-(mount.core/defstate db 
-  :start (mg/get-db conn "mission-match"))
- 
+      (start [component]
+        (println "Starting Connectio")
+        (let [conn (mg/connect) ]
+          (assoc component :conn conn)))
+      
+      (stop [component]
+        (println "Stopping Database")))
 
-(defrecord Database []
-  component/Lifecycle
 
-  (start [component]
-    (println "Starting Database")
-    (let [conn (mg/connect)]
-      (assoc component :conn conn))))
+    (defrecord Database [conn]
+      component/Lifecycle
+
+      (start [component]
+        (println "Starting Database")
+        (let [db (mg/get-db (:conn conn) "mission-match")]
+          (assoc component :db db))))
+
+    (defn new-connection  []
+        (map->Connection  {}))
+
+    (defn new-database  []
+        (map->Database  {}))
+
+    (defn db-system []
+      (component/system-map
+        :conn (new-connection)
+        :db (component/using
+              (new-database)
+              [:conn])))
+
+    (def sys (atom (db-system)))
+    
+    (defn start! []
+      (reset! sys (component/start @sys)))
+
+    (defn stop! []
+      (reset! sys (component/stop @sys)))
+
+    (start!)
+
+
+    (defn get-db []
+      (:db (:db @sys)))
+
+    (defn test-db-system []
+      (mc/insert (get-db) "test" {:text "this is a db-system-test"})
+      (if
+        (= (:text (mc/find-one-as-map (get-db) "test" {}))
+          "this is a db-system-test")
+        (println "db-system-test passes")
+        (println "db-system-test fails")))
